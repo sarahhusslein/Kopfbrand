458f4da2dcc1d16fce15959d6351455ad4e938ce
Sarah  Hußlein <sarahhusslein@MacBook-Air-von-Sarah.fritz.box>
2025-02-06 14:24:03 +0100

    contact

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,48 @@
+"use client"
+import { useEffect, useRef, useState } from 'react';
+
 import NavigationBar from "@/components/00_NavigationBar/navigationBar";
+import Contact from "@/components/07_Contact/contact";
+
+
 import styles from "./page.module.css";
 
 
 
 export default function Home() {
 
+  /****** States ******/
+  // Ref States
+  const containerRef = useRef(null);
+  const headerRef = useRef(null);
+  const servicesRef = useRef(null);
+  const numbersRef = useRef(null);
+  const casesHeadlineRef = useRef(null);
+  const casesOverviewRef = useRef(null);
+  const casesRef = useRef(null);
+  const teamRef = useRef(null);
+  const creativityRef = useRef(null);
+  const contactRef = useRef(null);
+  const footerRef = useRef(null);
+
+  // Height States
+  const [heights, setHeights] = useState([]);
+  const [totalHeight, setTotalHeight] = useState(0);
+
 
   return (
     <div>
       <div> Hallo </div>
       <NavigationBar />
+
+
+      <div 
+          id="contact"
+          ref={contactRef}
+          className={styles.contactContainer}
+        >
+          <Contact />
+      </div>
       
     </div>
   );
